1d7578301675edb2ab777df9f86bbffa5d31d809
eko5624 <562433403@qq.com>
2024-11-02 19:31:36 +0800

    -

---
+++ rime/自用五笔/lua/calculator.lua
@@ -0,0 +1,503 @@
+-- author: https://github.com/ChaosAlphard
+-- 说明 https://github.com/gaboolic/rime-shuangpin-fuzhuma/pull/41
+local T = {}
+
+function T.init(env)
+    local config = env.engine.schema.config
+    -- env.name_space = env.name_space:gsub('^*', '')
+    local _calc_pat = config:get_string("recognizer/patterns/calculator") or nil
+    -- 获取 recognizer/patterns/calculator 的第 2 个字符作为触发前缀（也就是获取等于号 = 或其他自定义字符）
+    T.prefix = _calc_pat and _calc_pat:sub(2, 2)
+    -- T.tips = "计算器"
+end
+
+local function startsWith(str, start)
+    return string.sub(str, 1, string.len(start)) == start
+end
+
+-- 函数表
+local calc_methods = {
+    -- e, exp(1) = e^1 = e
+    e = math.exp(1),
+    -- π
+    pi = math.pi,
+    b = 10 ^ 2,
+    q = 10 ^ 3,
+    k = 10 ^ 3,
+    w = 10 ^ 4,
+    tw = 10 ^ 5,
+    m = 10 ^ 6,
+    tm = 10 ^ 7,
+    y = 10 ^ 8,
+    g = 10 ^ 9
+}
+
+local methods_desc = {
+    ["e"] = "自然常数, 欧拉数",
+    ["pi"] = "圆周率 π",
+    ["b"] = "百",
+    ["q"] = "千",
+    ["k"] = "千",
+    ["w"] = "万",
+    ["tw"] = "十万",
+    ["m"] = "百万",
+    ["tm"] = "千万",
+    ["y"] = "亿",
+    ["g"] = "十亿"
+}
+
+-- random([m [,n ]]) 返回m-n之间的随机数, n为空则返回1-m之间, 都为空则返回0-1之间的小数
+local function random(...) return math.random(...) end
+-- 注册到函数表中
+calc_methods["rdm"] = random
+methods_desc["rdm"] = "随机数"
+
+-- 正弦
+local function sin(x) return math.sin(x) end
+calc_methods["sin"] = sin
+methods_desc["sin"] = "正弦"
+
+-- 双曲正弦
+local function sinh(x)
+    return (math.exp(x) - math.exp(-x)) / 2
+end
+calc_methods["sinh"] = sinh
+methods_desc["sinh"] = "双曲正弦"
+
+-- 反正弦
+local function asin(x) return math.asin(x) end
+calc_methods["asin"] = asin
+methods_desc["asin"] = "反正弦"
+
+-- 余弦
+local function cos(x) return math.cos(x) end
+calc_methods["cos"] = cos
+methods_desc["cos"] = "余弦"
+
+-- 双曲余弦
+local function cosh(x)
+    return (math.exp(x) + math.exp(-x)) / 2
+end
+calc_methods["cosh"] = cosh
+methods_desc["cosh"] = "双曲余弦"
+
+-- 反余弦
+local function acos(x) return math.acos(x) end
+calc_methods["acos"] = acos
+methods_desc["acos"] = "反余弦"
+
+-- 正切
+local function tan(x) return math.tan(x) end
+calc_methods["tan"] = tan
+methods_desc["tan"] = "正切"
+
+-- 双曲正切
+local function tanh(x)
+    local e = math.exp(2 * x)
+    return (e - 1) / (e + 1)
+end
+calc_methods["tanh"] = tanh
+methods_desc["tanh"] = "双曲正切"
+
+-- 反正切
+local function atan(x) return math.atan(x) end
+calc_methods["atan"] = atan
+methods_desc["atan"] = "反正切"
+
+-- 返回以弧度为单位的点(x,y)相对于x轴的逆时针角度。y是点的纵坐标，x是点的横坐标
+-- 返回范围从−π到π （以弧度为单位），其中负角度表示向下旋转，正角度表示向上旋转
+-- 它与传统的 math.atan(y/x) 函数相比，具有更好的数学定义，因为它能够正确处理边界情况（例如x=0）
+local function atan2(y, x)
+    if x == 0 and y == 0 then
+        return 0 / 0 -- 返回NaN
+    elseif x == 0 and y ~= 0 then
+        if y > 0 then
+            return math.pi / 2
+        else
+            return -math.pi / 2
+        end
+    else
+        return math.atan(y / x) + (x < 0 and math.pi or 0)
+    end
+end
+calc_methods["atan2"] = atan2
+methods_desc["atan2"] = "返回以弧度为单位的点(x,y)相对于x轴的逆时针角度"
+
+-- 将角度从弧度转换为度 e.g. deg(π) = 180
+local function deg(x) return math.deg(x) end
+calc_methods["deg"] = deg
+methods_desc["deg"] = "弧度转换为角度"
+
+-- 将角度从度转换为弧度 e.g. rad(180) = π
+local function rad(x) return math.rad(x) end
+calc_methods["rad"] = rad
+methods_desc["rad"] = "角度转换为弧度"
+
+-- 返回两个值, 无法参与运算后续
+-- 返回m,e 使得x = m*2^e
+--[[
+local function frexp(x)
+  return math.frexp(x)
+end
+calcPlugin["frexp"] = frexp
+--]]
+
+-- 返回 x*2^y
+local function ldexp(x, y) return x * 2 ^ y end
+calc_methods["ldexp"] = ldexp
+methods_desc["ldexp"] = "返回 x*2^y"
+
+-- 返回 e^x
+local function exp(x) return math.exp(x) end
+calc_methods["exp"] = exp
+methods_desc["exp"] = "返回 e^x"
+
+-- 计算开 N 次方
+local function nth_root(x, n)
+    if n % 2 == 0 and x < 0 then
+        return nil -- 偶次方时负数没有实数解
+    elseif x < 0 then
+        return -((-x) ^ (1 / n))
+    else
+        return x ^ (1 / n)
+    end
+end
+calc_methods["nroot"] = nth_root
+methods_desc["nroot"] = "计算 x 开 N 次方"
+
+-- 返回x的平方根 e.g. sqrt(x) = x^0.5
+local function sqrt(x) return math.sqrt(x) end
+calc_methods["sqrt"] = sqrt
+methods_desc["sqrt"] = "计算 x 平方根"
+
+-- x为底的对数, log(10, 100) = log(100) / log(10) = 2
+local function log(x, y)
+    -- 不能为负数或0
+    if x <= 0 or y <= 0 then
+        return nil
+    end
+
+    return math.log(y) / math.log(x)
+end
+calc_methods["log"] = log
+methods_desc["log"] = "x作为底数的对数"
+
+-- 自然数e为底的对数
+local function loge(x)
+    -- 不能为负数或0
+    if x <= 0 then return nil end
+
+    return math.log(x)
+end
+calc_methods["loge"] = loge
+methods_desc["loge"] = "e作为底数的对数"
+
+-- 10为底的对数
+local function logt(x)
+    -- 不能为负数或0
+    if x <= 0 then return nil end
+
+    return math.log(x) / math.log(10)
+end
+calc_methods["logt"] = logt
+methods_desc["logt"] = "10作为底数的对数"
+
+-- 平均值
+local function avg(...)
+    local data = {...}
+    local n = select("#", ...)
+    -- 样本数量不能为0
+    if n == 0 then return nil end
+
+    -- 计算总和
+    local sum = 0
+    for _, value in ipairs(data) do
+        sum = sum + value
+    end
+
+    return sum / n
+end
+calc_methods["avg"] = avg
+methods_desc["avg"] = "平均值"
+
+-- 方差
+local function variance(...)
+    local data = {...}
+    local n = select("#", ...)
+    -- 样本数量不能为0
+    if n == 0 then return nil end
+
+    -- 计算均值
+    local sum = 0
+    for _, value in ipairs(data) do
+        sum = sum + value
+    end
+    local mean = sum / n
+
+    -- 计算方差
+    local sum_squared_diff = 0
+    for _, value in ipairs(data) do
+        sum_squared_diff = sum_squared_diff + (value - mean) ^ 2
+    end
+
+    return sum_squared_diff / n
+end
+calc_methods["var"] = variance
+methods_desc["var"] = "方差"
+
+-- 阶乘
+local function factorial(x)
+    -- 不能为负数
+    if x < 0 then return nil end
+    if x == 0 or x == 1 then return 1 end
+
+    local result = 1
+    for i = 1, x do
+        result = result * i
+    end
+
+    return result
+end
+calc_methods["fact"] = factorial
+methods_desc["fact"] = "阶乘"
+
+-- 实现阶乘计算(!)
+local function replaceToFactorial(str)
+    -- 替换[0-9]!字符为fact([0-9])以实现阶乘
+    return str:gsub("([0-9]+)!", "fact(%1)")
+end
+
+local function serialize(obj)
+    local type = type(obj)
+    if type == "number" then
+        return isinteger(obj) and floor(obj) or obj
+    elseif type == "boolean" then
+    	return tostring(obj)
+  	elseif type == "string" then
+    	return '"'..obj..'"'
+  	elseif type == "table" then
+    	local str = "{"
+    	local i = 1
+    	for k, v in pairs(obj) do
+      		if i ~= k then  
+        		str = str.."["..serialize(k).."]="
+      		end
+      		str = str..serialize(v)..", "  
+      		i = i + 1
+    	end
+    	str = str:len() > 3 and str:sub(0,-3) or str
+    	return str.."}"
+  	elseif pcall(obj) then -- function類型
+    	return "callable"
+  	end
+  	return obj
+end
+
+local function speakLiterally(str, valMap)
+	valMap = valMap or {
+		[0]="零"; "一"; "二"; "三"; "四"; "五"; "六"; "七"; "八"; "九"; "十";
+		["+"]="正"; ["-"]="负"; ["."]="点"; [""]=""
+	}
+
+	local tbOut = {}
+	for k = 1, #str do
+		local v = string.sub(str, k, k)
+		v = tonumber(v) or v
+		tbOut[k] = valMap[v]
+	end
+	return table.concat(tbOut)
+end
+
+local function speakMillitary(str)
+	return speakLiterally(str, {[0]="洞"; "幺"; "两"; "三"; "四"; "五"; "六"; "拐"; "八"; "勾"; "十";["+"]="正"; ["-"]="负"; ["."]="点"; [""]=""})
+end
+
+local function splitNumStr(str)
+	--[[
+		split a number (or a string describing a number) into 4 parts:
+		.sym: "+", "-" or ""
+		.int: "0", "000", "123456", "", etc
+		.dig: "." or ""
+		.dec: "0", "10000", "00001", "", etc
+	--]]
+	local part = {}
+	part.sym, part.int, part.dig, part.dec = string.match(str, "^([%+%-]?)(%d*)(%.?)(%d*)")
+	return part
+end
+
+local function speakBar(str, posMap, valMap)
+	posMap = posMap or {[1]="仟"; [2]="佰"; [3]="拾"; [4]=""}
+	valMap = valMap or {[0]="零"; "一"; "二"; "三" ;"四"; "五"; "六"; "七"; "八"; "九"} -- the length of valMap[0] should not excess 1
+
+	local out = ""
+	local bar = string.sub("****" .. str, -4, -1) -- the integer part of a number string can be divided into bars; each bar has 4 bits
+	for pos = 1, 4 do
+		local val = tonumber(string.sub(bar, pos, pos))
+		-- case1: place holder
+		if val == nil then
+			goto continue
+		end
+		-- case2: number 1~9
+		if val > 0 then
+			out = out .. valMap[val] .. posMap[pos]
+			goto continue
+		end
+		-- case3: number 0
+		local valNext = tonumber(string.sub(bar, pos+1, pos+1))
+		if ( valNext==nil or valNext==0 )then
+			goto continue
+		else
+			out = out .. valMap[0]
+			goto continue
+		end
+	::continue::
+	end
+	if out == "" then out = valMap[0] end
+	return out
+end
+
+local function speakIntOfficially(str, posMap, valMap)
+	posMap = posMap or {[1]="千"; [2]="百"; [3]="十"; [4]=""}
+	valMap = valMap or {[0]="零"; "一"; "二"; "三" ;"四"; "五"; "六"; "七"; "八"; "九"} -- the length of valMap[0] should not excess 1
+
+	-- split the number string into bars, for example, in:str=123456789 → out:tbBar={1|2345|6789}
+	local int = string.match(str, "^0*(%d+)$")
+	if int=="" then int = "0" end
+	local remain = #int % 4
+	if remain==0 then remain = 4 end
+	local tbBar = {[1] = string.sub(int, 1, remain)}
+	for pos = remain+1, #int, 4 do
+		local bar = string.sub(int, pos, pos+3)
+		table.insert(tbBar, bar)
+	end
+	-- generate the suffixes of each bar, for example, tbSpeakBarSuffix={亿|万|""}
+	local tbSpeakBarSuffix = {[1]=""}
+	for iBar = 2, #tbBar do
+		local suffix = (iBar % 2 == 0) and ("万"..tbSpeakBarSuffix[1]) or ("亿"..tbSpeakBarSuffix[2])
+		table.insert(tbSpeakBarSuffix, 1, suffix)
+	end
+	-- speak each bar
+	local tbSpeakBar = {}
+	for k = 1, #tbBar do
+		tbSpeakBar[k] = speakBar(tbBar[k], posMap, valMap)
+	end
+	-- combine the results
+	local out = ""
+	for k = 1, #tbBar do
+		local speakBar = tbSpeakBar[k]
+		if speakBar ~= valMap[0] then
+			out = out .. speakBar .. tbSpeakBarSuffix[k]
+		end
+	end
+	if out == "" then out = valMap[0] end
+	return out
+end
+
+local function speakDecMoney(str, posMap, valMap)
+	posMap = posMap or {[1]="角"; [2]="分"; [3]="厘"; [4]="毫"}
+	valMap = valMap or {[0]="零"; "壹"; "贰"; "叁" ;"肆"; "伍"; "陆"; "柒"; "捌"; "玖"} -- the length of valMap[0] should not excess 1
+
+	local dec = string.sub(str, 1, 4)
+	dec = string.gsub(dec, "0*$", "")
+	if dec == "" then
+		return "整"
+	end
+
+	local out = ""
+	for pos = 1, #dec do
+		local val = tonumber(string.sub(dec, pos, pos))
+		out = out .. valMap[val] .. posMap[pos]
+	end
+	return out
+end
+
+local function speakOfficiallyLower(str)
+	local part = splitNumStr(str)
+	local speakSym = speakLiterally(part.sym)
+	local speakInt = speakIntOfficially(part.int)
+	local speakDig = speakLiterally(part.dig)
+	local speakDec = speakLiterally(part.dec)
+	local out = speakSym .. speakInt .. speakDig .. speakDec
+	return out
+end
+
+local function speakOfficiallyUpper(str)
+    local part = splitNumStr(str)
+	local speakSym = speakLiterally(part.sym)
+	local speakInt = speakIntOfficially(part.int, {[1]="仟"; [2]="佰"; [3]="拾"; [4]=""}, {[0]="零"; "壹"; "贰"; "叁" ;"肆"; "伍"; "陆"; "柒"; "捌"; "玖"})
+	local speakDig = speakLiterally(part.dig)
+	local speakDec = speakLiterally(part.dec)
+	local out = speakSym .. speakInt .. speakDig .. speakDec
+	return out
+end
+
+local function speakMoneyUpper(str)
+	local part = splitNumStr(str)
+	local speakSym = speakLiterally(part.sym)
+	local speakInt = speakIntOfficially(part.int, {[1]="仟"; [2]="佰"; [3]="拾"; [4]=""}, {[0]="零"; "壹"; "贰"; "叁" ;"肆"; "伍"; "陆"; "柒"; "捌"; "玖"}) .. "元"
+	local speakDec = speakDecMoney(part.dec)
+	local out = speakSym .. speakInt .. speakDec
+	return out
+end
+
+local function speakMoneyLower(str)
+	local part = splitNumStr(str)
+	local speakSym = speakLiterally(part.sym)
+	local speakInt = speakIntOfficially(part.int, {[1]="千"; [2]="百"; [3]="十"; [4]=""}, {[0]="〇"; "一"; "二"; "三" ;"四"; "五"; "六"; "七"; "八"; "九"}) .. "元"
+	local speakDec = speakDecMoney(part.dec)
+	local out = speakSym .. speakInt .. speakDec
+	return out
+end
+
+local function baseConverse(str, from, to)
+	local str10 = str
+	if from == 16 then
+		str10 = string.format("%d", str)
+	end
+	local strout = str10
+	if to == 16 then
+		strout = string.format("%#x", str10)
+	end
+	return strout
+end
+
+-- 简单计算器
+function T.func(input, seg, env)
+    local composition = env.engine.context.composition
+    if composition:empty() then return end
+    local segment = composition:back()
+
+    if startsWith(input, T.prefix) or (seg:has_tag("calculator")) then
+        -- segment.prompt = "〔" .. T.tips .. "〕"
+        -- 提取算式
+        local express = input:gsub(T.prefix, "")
+        -- 算式长度 = 0 直接终止(只打出 = 没有计算意义)
+        if (string.len(express) == 0) and (not calc_methods[express]) then return end
+        -- if (string.len(express) == 2) and (express:match("^%d[^%!]$")) then return end
+        local code = replaceToFactorial(express)
+
+        local loaded_func, load_error = load("return " .. code, "calculate", "t", calc_methods)
+        if loaded_func and (type(methods_desc[code]) == "string") then
+            yield(Candidate(input, seg.start, seg._end, express .. ":" .. methods_desc[code], ""))
+            	elseif loaded_func then
+            local success, result = pcall(loaded_func)
+            if success then
+                yield(Candidate(input, seg.start, seg._end, express .. "=" .. tostring(result), "〈等式〉"))
+                yield(Candidate(input, seg.start, seg._end, tostring(result), "〈结果〉"))
+			    yield(Candidate("number", seg.start, seg._end, speakMoneyUpper(result), "〈大写金额〉"))
+			    yield(Candidate("number", seg.start, seg._end, speakMoneyLower(result), "〈小写金额〉"))
+			    yield(Candidate("number", seg.start, seg._end, speakOfficiallyUpper(result), "〈大写数字〉"))
+			    yield(Candidate("number", seg.start, seg._end, speakOfficiallyLower(result), "〈小写数字〉"))
+            else
+                -- 处理执行错误
+        		yield(Candidate(input, seg.start, seg._end, express, "执行错误"))
+            end
+        else
+            -- 处理加载错误
+      		yield(Candidate(input, seg.start, seg._end, express, ""))
+        end
+    end
+end
+
+return T
+
---
+++ rime/自用五笔/rime.lua
@@ -0,0 +1,396 @@
+﻿--[
+--modify: 空山明月
+--date: 2024-04-03	
+--]
+
+-- --=========================================================关键字修改--==========================================================
+-- --==========================================================--==========================================================
+rv_var={ week_var="week",date_var="date",nl_var="nl",time_var="time",jq_var="jq"}	-- 编码关键字修改
+single_keyword="single_char"	-- 单字过滤switcher参数
+-- --==========================================================--==========================================================
+-- --==========================================================--==========================================================
+require("lunarDate")
+require("lunarJq")
+require("lunarGz")
+--===================================================时间／日期／农历／历法／数字转换输出=================================================================
+-- --====================================================================================================================
+function CnDate_translator(y)
+	 local t,cstr,t2,t1
+	 cstr = {"〇","一","二","三","四","五","六","七","八","九"}  t=""  t1=tostring(y)
+	if t1.len(tostring(t1))~=8 then return t1 end
+	 for i =1,t1.len(t1) do
+		  t2=cstr[tonumber(t1.sub(t1,i,i))+1]
+		  if i==5 and t2 ~= "〇" then t2="年十" elseif i==5 and t2 == "〇" then t2="年"  end
+		  if i==6 and t2 ~= "〇" then t2 =t2 .. "月" elseif i==6 and t2 == "〇" then t2="月"  end
+		  --if t.sub(t,t.len(t)-1)=="年" then t2=t2 .. "月" end
+		  if i==7 and tonumber(t1.sub(t1,7,7))>1 then t2= t2 .. "十" elseif i==7 and t2 == "〇" then t2="" elseif i==7 and tonumber(t1.sub(t1,7,7))==1 then t2="十" end
+		  if i==8 and t2 ~= "〇" then t2 =t2 .. "日" elseif i==8 and t2 == "〇" then t2="日"  end
+		  t=t .. t2
+	end
+		  return t
+end
+
+local GetLunarSichen= function(time,t)
+	local time=tonumber(time)
+	local LunarSichen = {"子时(夜半｜三更)", "丑时(鸡鸣｜四更)", "寅时(平旦｜五更)", "卯时(日出)", "辰时(食时)", "巳时(隅中)", "午时(日中)", "未时(日昳)", "申时(哺时)", "酉时(日入)", "戌时(黄昏｜一更)", "亥时(人定｜二更)"}
+	if tonumber(t)==1 then sj=math.floor((time+1)/2)+1 elseif tonumber(t)==0 then sj=math.floor((time+13)/2)+1 end
+	if sj>12 then return LunarSichen[sj-12] else return LunarSichen[sj] end
+end
+
+--年天数判断
+local function IsLeap(y)
+	local year=tonumber(y)
+	if math.fmod(year,400)~=0 and math.fmod(year,4)==0 or math.fmod(year,400)==0 then return 366
+	else return 365 end
+end
+
+local format_Time= function()
+	if os.date("%p")=="AM" then return "上午" else return "下午" end
+end
+
+-- 星期格式转换
+local format_week= function(n)
+	local obj={"日","一","二","三","四","五","六"}
+	if tonumber(n)==1 then return "周"..obj[os.date("%w")+1] else return "星期"..obj[os.date("%w")+1] end
+end
+------------------------lua内置日期变量参考-------------------------------------
+--[[
+	--%a 星期简称，如Wed	%A 星期全称，如Wednesday
+	--%b 月份简称，如Sep	%B 月份全称，如September
+	--%c 日期时间格式 (e.g., 09/16/98 23:48:10)
+	--%d 一个月的第几天 [01-31]	%j 一年的第几天
+	--%H 24小时制 [00-23]	%I 12小时制 [01-12]
+	--%M 分钟 [00-59]	%m 月份 (09) [01-12]
+	--%p 上午/下午 (pm or am)
+	--%S 秒 (10) [00-61]
+	--%w 星期的第几天 [0-6 = Sunday-Saturday]	%W 一年的第几周
+	--%x 日期格式 (e.g., 09/16/98)	%X 时间格式 (e.g., 23:48:10)
+	--%Y 年份全称 (1998)	%y 年份简称 [00-99]
+	--%% 百分号
+	--os.date() 把时间戳转化成可显示的时间字符串
+	--os.time ([table])
+--]]
+----------------------------------------------------------------
+
+-- 公历日期
+function date_translator(input, seg)
+	local keyword = rv_var["date_var"]
+	if (input == keyword) then
+		local dates = {
+			os.date("%Y年%m月%d日")
+			,os.date("%Y-%m-%d")
+			,os.date("%Y-%m-%d 第%W周")
+			-- ,os.date("%Y.%m.%d")
+			-- ,os.date("%Y%m%d")
+			-- ,CnDate_translator(os.date("%Y%m%d"))
+			-- ,os.date("%Y-%m-%d｜%j/" .. IsLeap(os.date("%Y")))
+			}
+		-- Candidate(type, start, end, text, comment)
+		for i =1,#dates do
+			local gregorian_date = (Candidate(keyword, seg.start, seg._end, dates[i], "〈日期〉"))
+			gregorian_date.quality = 999
+			yield(gregorian_date)
+		end
+		dates = nil
+	end
+end
+
+-- 公历时间
+function time_translator(input, seg)
+	local keyword = rv_var["time_var"]
+	if (input == keyword) then
+		local times = {
+			os.date("%Y年%m月%d日 %H:%M:%S")
+			,os.date("%Y-%m-%d " .. format_Time() .. "%I:%M")
+			}
+		for i =1,#times do
+			local current_time = (Candidate(keyword, seg.start, seg._end, times[i], "〈时间〉"))
+			current_time.quality = 999
+			yield(current_time)
+		end
+		times = nil
+	end
+end
+
+-- 农历日期
+function lunar_translator(input, seg)
+	local keyword = rv_var["nl_var"]
+	if (input == keyword) then
+		local lunar = {
+				{Date2LunarDate(os.date("%Y%m%d")) .. JQtest(os.date("%Y%m%d")),"〈公历⇉农历〉"}
+				,{Date2LunarDate(os.date("%Y%m%d")) .. GetLunarSichen(os.date("%H"),1),"〈公历⇉农历〉"}
+				,{lunarJzl(os.date("%Y%m%d%H")),"〈公历⇉干支〉"}
+				,{LunarDate2Date(os.date("%Y%m%d"),0),"〈农历⇉公历〉"}
+			}
+		local leapDate={LunarDate2Date(os.date("%Y%m%d"),1).."（闰）","〈农历⇉公历〉"}
+		if string.match(leapDate[1],"^(%d+)")~=nil then table.insert(lunar,leapDate) end
+		for i =1,#lunar do
+			local lunar_ymd = (Candidate(keyword, seg.start, seg._end, lunar[i][1], lunar[i][2]))
+			lunar_ymd.quality = 999
+			yield(lunar_ymd)
+		end
+		lunar = nil
+	end
+end
+
+local function QueryLunarInfo(date)
+	local str,LunarDate,LunarGz,result,DateTime
+	date=tostring(date) result={}
+	str = date:gsub("^(%u+)","")
+	if string.match(str,"^(20)%d%d+$")~=nil or string.match(str,"^(19)%d%d+$")~=nil then
+		if string.len(str)==4 then str=str..string.sub(os.date("%m%d%H"),1) elseif string.len(str)==5 then str=str..string.sub(os.date("%m%d%H"),2) elseif string.len(str)==6 then str=str..string.sub(os.date("%m%d%H"),3) elseif string.len(str)==7 then str=str..string.sub(os.date("%m%d%H"),4)
+		elseif string.len(str)==8 then str=str..string.sub(os.date("%m%d%H"),5) elseif string.len(str)==9 then str=str..string.sub(os.date("%m%d%H"),6) else str=string.sub(str,1,10) end
+		if tonumber(string.sub(str,5,6))>12 or tonumber(string.sub(str,5,6))<1 or tonumber(string.sub(str,7,8))>31 or tonumber(string.sub(str,7,8))<1 or tonumber(string.sub(str,9,10))>24 then return result end
+		LunarDate=Date2LunarDate(str)  LunarGz=lunarJzl(str)  DateTime=LunarDate2Date(str,0)
+		if LunarGz~=nil then
+			result={
+				{CnDate_translator(string.sub(str,1,8)),"〈中文日期〉"}
+				,{LunarDate,"〈公历⇉农历〉"}
+				,{LunarGz,"〈公历⇉干支〉"}
+			}
+			if tonumber(string.sub(str,7,8))<31 then
+				table.insert(result,{DateTime,"〈农历⇉公历〉"})
+				local leapDate={LunarDate2Date(str,1).."（闰）","〈农历⇉公历〉"}
+				if string.match(leapDate[1],"^(%d+)")~=nil then table.insert(result,leapDate) end
+			end
+		end
+	end
+
+	return result
+end
+
+-- 农历查询
+function QueryLunar_translator(input, seg)	--输入一个或一个以上等于号，进行农历反查
+	local str,lunar
+	if string.match(input,"^=+(%d+)$")~=nil then
+		str = string.gsub(input,"^(=+)", "")
+		if string.match(str,"^(20)%d%d+$")~=nil or string.match(str,"^(19)%d%d+$")~=nil then
+			lunar=QueryLunarInfo(str)
+			if #lunar>0 then
+				for i=1,#lunar do
+					yield(Candidate(input, seg.start, seg._end, lunar[i][1],lunar[i][2]))
+				end
+			end
+		end
+	end
+end
+
+--- 单字模式
+function single_char(input, env)
+	local b = env.engine.context:get_option(single_keyword)
+	local input_text = env.engine.context.input
+	for cand in input:iter() do
+		if (not b or utf8.len(cand.text) == 1 or table.vIn(rv_var, input_text) or input_text:find("^z") or input_text:find("^[%u%p]")) then
+			yield(cand)
+		end
+	end
+end
+
+-- 星期
+function week_translator(input, seg)
+	local keyword = rv_var["week_var"]
+	-- local luapath=debug.getinfo(1,"S").source:sub(2):sub(1,-9)   -- luapath.."lua\\user.txt"
+	if (input == keyword) then
+		local weeks = {
+			format_week(1)
+			,format_week(2)
+			,format_week(0)
+			}
+		for i =1,#weeks do
+			yield(Candidate(keyword, seg.start, seg._end, weeks[i], "〈星期〉"))
+		end
+		weeks = nil
+	end
+end
+
+--列出当年余下的节气
+function Jq_translator(input, seg)
+	local keyword = rv_var["jq_var"]
+	if (input == keyword) then
+		local jqs = GetNowTimeJq(os.date("%Y%m%d"))
+		for i =1,#jqs do
+			local lunar_jq = (Candidate(keyword, seg.start, seg._end, jqs[i], "〈节气〉"))
+			lunar_jq.quality = 999
+			yield(lunar_jq)
+		end
+		jqs = nil
+	end
+end
+
+-------------------------------------------------------------
+
+-- 时间向前或向后计算
+-- author: 空山明月
+local function addDaysToDate(days, format)
+    return os.date(format, os.time() + days * 86400)
+end
+
+-- 从当前日期向前或向后计算
+-- author: 空山明月
+function somedate_translator(input, seg, days)
+	local keyword = rv_var["date_var"]
+	if (input == keyword) then
+		local dates = {
+			addDaysToDate(days, "%Y年%m月%d日")
+			,addDaysToDate(days, "%Y-%m-%d")
+			,addDaysToDate(days, "%Y%m%d")
+			}
+		for i =1,#dates do
+			yield(Candidate(keyword, seg.start, seg._end, dates[i], "〈日期〉"))
+		end
+		dates = nil
+	end
+end
+
+-- 获取本月相邻月份同一天时的日期
+-- 比如今天是 2024-05-13，则可获取 2024-04/6-13 的日期
+-- today: 当天日期
+-- is_next: true 表示获取下个月，fase 表示获取上个月
+-- retrun: 返回结果表示与当天相差的天数
+-- author: 空山明月
+function get_month_sameday(is_next)
+	local offset_days = 0
+	local this_year, this_month = os.date("%Y", os.time()), os.date("%m", os.time())
+	local now_days = os.date("%d", os.time())  -- 本月第几天
+	
+	local last_month, next_month = 0, 0
+    local this_day_amount = 0
+	local last_day_amount = 0
+	local next_day_amount = 0
+
+	if is_next then
+		-- 如果现在是12月份，需要向后推一年
+		if this_month == 12 then
+			last_month, next_month = this_month - 1, 1
+		else
+			last_month, next_month = this_month - 1, this_month + 1
+		end
+
+        this_day_amount = os.date("%d", os.time({year=this_year, month=this_month+1, day=0}))
+	    next_day_amount = os.date("%d", os.time({year=this_year, month=next_month+1, day=0}))	
+
+        -- 如果时间间隔超出了下个月的最后一天，则按最后一天算
+        local temp_offset_max = this_day_amount
+        local temp_offset_min = this_day_amount - now_days + next_day_amount
+        if now_days >= next_day_amount then
+            offset_days = temp_offset_min
+        else
+            offset_days = temp_offset_max
+        end
+	else
+		-- 如果当前是1月份，需要向前推一年
+		if this_month == 1 then
+			last_month, next_month = 12, this_month + 1
+		else
+			last_month, next_month = this_month - 1, this_month + 1
+		end
+
+        this_day_amount = os.date("%d", os.time({year=this_year, month=this_month+1, day=0}))
+	    last_day_amount = os.date("%d", os.time({year=this_year, month=last_month+1, day=0}))	
+
+        -- 如果时间间隔超出了下个月的最后一天，则按最后一天算
+        if now_days <= last_day_amount then
+            offset_days = last_day_amount
+        else
+            offset_days = now_days
+        end
+	end
+    
+	return offset_days
+end
+
+-- 时期类字符串集
+-- author: 空山明月
+str_date_time={ 
+	today="wygd",
+	next_day="jegd",
+	after_next_day = "rggd",
+	lastday = "jtgd",
+	before_lastday = "uegd",
+	time = "jfuj",
+	this_week = "sgmf",
+	last_week = "hhmf",
+	next_week = "ghmf",
+	this_month = "sgee",
+	last_month = "hhee",
+	next_month = "ghee",
+	}
+
+-- 时间字符串转译成时间
+-- author: 空山明月
+function str2datetime_translator(input, seg)
+
+	-- 输出今天的日期
+	if (input == str_date_time["today"]) then
+		date_translator("date", seg)
+	end
+
+	-- 输出明天的日期
+	if (input == str_date_time["next_day"]) then
+		somedate_translator("date", seg, 1)
+	end
+
+	-- 输出后天的日期
+	if (input == str_date_time["after_next_day"]) then
+		somedate_translator("date", seg, 2)
+	end
+
+	-- 输出昨天的日期
+	if (input == str_date_time["lastday"]) then
+		somedate_translator("date", seg, -1)
+	end
+
+	-- 输出前天的日期
+	if (input == str_date_time["before_lastday"]) then
+		somedate_translator("date", seg, -2)
+	end
+
+	-- 输出当前时间
+	if (input == str_date_time["time"]) then
+		time_translator("time", seg)
+	end
+
+	-- 输出本周时间：表示本周的当天时间
+	if (input == str_date_time["this_week"]) then
+		date_translator("date", seg)
+	end
+
+	-- 输出上周时间：表示上周对应星期时间
+	-- 比如今天是周三，则此函数返回上周周三对应的日期
+	if (input == str_date_time["last_week"]) then
+		somedate_translator("date", seg, -7)
+	end
+
+	-- 输出下周时间：表示上周对应星期时间
+	-- 比如今天是周三，则此函数返回下周周三对应的日期
+	if (input == str_date_time["next_week"]) then
+		somedate_translator("date", seg, 7)
+	end
+
+	-- 输出本月日期，默认是本月当天日期
+	if (input == str_date_time["this_month"]) then
+		date_translator("date", seg)
+	end
+
+	-- 输出上月与当天天数相同的日期，有末则按最后一天计算
+	if (input == str_date_time["last_month"]) then
+		local days_offset = get_month_sameday(false)
+		somedate_translator("date", seg, -days_offset)
+	end
+
+	-- 输出下月与当天天数相同的日期，有末则按最后一天计算
+	if (input == str_date_time["next_month"]) then
+		local days_offset = get_month_sameday(true)
+		somedate_translator("date", seg, days_offset)
+	end
+end
+
+function time_date(input, seg, env)
+	date_translator(input, seg)
+	time_translator(input, seg)
+	week_translator(input, seg)
+	lunar_translator(input, seg)
+	Jq_translator(input, seg)
+	QueryLunar_translator(input, seg)
+	str2datetime_translator(input, seg)
+end
