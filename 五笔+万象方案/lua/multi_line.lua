ad9ba98df6adb186bc8bb2313d97ed5f4245b62c
eko5624 <562433403@qq.com>
2026-08-05 17:48:30 +0800

    multi_line.lua: 继续精简
    去掉[[...]]和\z转义

--- 五笔+万象方案/lua/multi_line.lua
+++ 五笔+万象方案/lua/multi_line.lua
@@ -1,14 +1,13 @@
 -- @amzxyz  https://github.com/amzxyz/rime-wanxiang
 -- @modify: eko5624
 
--- 支持：\n \r \t \s \z、[[字面区块]]、字符\重复次数
+-- 支持：\n \r \t \s、字符\重复次数
 ----------------------------------------------------
 -- 数量重复 ( 字符 + \ + 数字 )
 -- a\3 => aaa     哈\2 => 哈哈     !\5 => !!!!!
 
 -- 基础转义
 -- \n : 换行符    \s : 空格    \t : 制表符
--- [[...]] : 区块内不转义 (如: [[\Y]] 输出 \Y)
 
 --  示例: 吧	b	5
 -- 示例: 哈\5	hhhhh	5
@@ -27,31 +26,23 @@ local escape_map = {
     ["\\r"] = "\r",
     ["\\t"] = "\t",
     ["\\s"] = " ",
-    ["\\z"] = zwsp,
 }
 
 local utf8_char_pattern = "[%z\1-\127\194-\244][\128-\191]*"
 
 -- 核心转义处理
-local function apply_escape_fast(text)
+local function apply_escape(text)
     if not text or not find(text, "\\", 1, true) then
         return text, false
     end
-    local blocks = {}
-    local s = text:gsub("%[%[(.-)%]%]", function(txt)
-        blocks[#blocks + 1] = txt
-        return "\0BLK" .. #blocks .. "\0"
-    end)
-    s = s:gsub("\\[ntrsz]", escape_map)
+    local s = text
+    s = s:gsub("\\[ntrs]", escape_map)
     s = s:gsub("(" .. utf8_char_pattern .. ")\\(%d+)", function(char, count)
         local n = tonumber(count)
         if n and n > 0 and n < 200 then
             return string.rep(char, n)
         end
         return char .. "\\" .. count
-    end)
-    s = s:gsub("\0BLK(%d+)\0", function(i)
-        return blocks[tonumber(i)] or ""
     end)
     return s, s ~= text
 end
@@ -62,7 +53,7 @@ local function process_candidate(cand)
     if not text or text == "" then
         return cand
     end
-    local t2, text_changed = apply_escape_fast(text)
+    local t2, text_changed = apply_escape(text)
     if text_changed then
         local nc = Candidate(cand.type, cand.start, cand._end, t2, cand.comment)
         nc.preedit = cand.preedit
